518a06b8a35da5117b5571cae3367a2977a78b5c
bashir <b.mihyar@wysada.com>
2018-02-28 19:15:29 +0200

    create form partial and display it

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-	  console.log("+++++++++++++ name: " + data);
+	  console.log("+++++++++++++ name: " + req.param('destinationCity'));
 	  res.render('pages/index', {data});
 	  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
