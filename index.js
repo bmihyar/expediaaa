51c14fdd48baad8bee4cace1a350902afea997e6
bashir <b.mihyar@wysada.com>
2018-02-28 11:40:27 +0200

    passing data to template file

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var options = {
     path: '/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel',
     headers: {'User-Agent': 'request'}
 };
+var data = "";
 
 https.get(options, function (res) {
     var json = '';
@@ -18,7 +19,7 @@ https.get(options, function (res) {
     res.on('end', function () {
         if (res.statusCode === 200) {
             try {
-                var data = JSON.parse(json);
+                data = JSON.parse(json);
                 // data is available here:
                 console.log("hOttellllll name: " + data.offers.Hotel[0].hotelInfo.hotelName);
             } catch (e) {
@@ -36,5 +37,5 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', data))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
