85076f54a3a4b258ba4bf4456cdf94016dc380ed
bashir <b.mihyar@wysada.com>
2018-03-01 10:05:55 +0200

    tracing ...

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-	var data = "";
+	var data;
 	console.log("+++++++++++++ name: " + req.params.destinationCity);
 	let  apis_url = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
 	if(req.params.destinationCity)
 		apis_url += "&destinationCity=" + req.params.destinationCity;
-	console.log("APIssss: " + apis_url);  
-	request.get({ url: apis_url }, (error, response, body) => { 
-		if (!error && response.statusCode == 200) { 
+
+	request.get({ url: apis_url }, function(error, response, body) { 
+		//if (!error && response.statusCode == 200) { 
 			try {
 				data = JSON.parse(body);
 				console.log("hOttellllll name: " + data.offers.Hotel[0].hotelInfo.hotelName);
 			} catch (e) {
 				console.log('Error parsing JSON!');
 			}
-		} else {
-			console.log('statusCode is not 200 !' + error);
-		}
+		//} else {
+			//console.log('statusCode is not 200 !' + error);
+		//}
     }); 
 	console.log("lolololo name: " + data);  
-	//res.render('pages/index', {data});
-	res.render('pages/index');
+	res.render('pages/index', {data});
 	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
